25d466d3ebfe4be0af45f786da30ae06a4c4917e
NZ-WEB <nzweb@vk.com>
2022-06-17 19:09:56 +0300

    feat(base-layout): add touch button
    Added AppTouchButton component

---
+++ src/components/AppTouchButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    class="touch-button"
+    :class="{ 'touch-button_active': isActive }"
+  ></button>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+export default class AppTouchButton extends Vue {
+  @Prop(Boolean)
+  readonly isActive!: boolean;
+}
+</script>
+
+<style lang="scss" scoped>
+.touch-button {
+  background: rgb(0, 131, 11);
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  border: 1px solid #000;
+
+  &_active {
+    background: rgb(255, 153, 0);
+  }
+}
+</style>
